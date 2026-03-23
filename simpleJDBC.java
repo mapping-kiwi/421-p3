5010af1c96b6bfb2911f2ce0a514bd12f63f9e1b
lince505 <linkys505@gmail.com>
2026-03-23 14:50:52 -0400

    worked more on the menu, added skeleton to the functions

--- simpleJDBC.java
+++ simpleJDBC.java
@@ -1,4 +1,5 @@
 import java.sql.* ;
+import java.util.Scanner;
 
 class simpleJDBC
 {
@@ -23,8 +24,8 @@ class simpleJDBC
         String url = "jdbc:db2://winter2026-comp421.cs.mcgill.ca:50000/comp421";
 
         //REMEMBER to remove your user id and password before submitting your code!!
-        String your_userid = "";
-        String your_password = "";
+        String your_userid = System.getenv("DB421_USER");
+        String your_password = System.getenv("DB421_PASSWORD");
         //AS AN ALTERNATIVE, you can just set your password in the shell environment in the Unix (as shown below) and read it from there.
         //$  export SOCSPASSWD=yoursocspasswd 
         if(your_userid == null && (your_userid = System.getenv("SOCSUSER")) == null)
@@ -139,6 +140,101 @@ class simpleJDBC
 //        System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
 //        System.out.println(e);
 //      }
+
+      /// Main Menu
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            // Display menu
+            System.out.println("\n===== DataBass Dashboard =====");
+            System.out.println("1) View and/or Process Booking Requests");
+            System.out.println("2) Create a new booking");
+            System.out.println("3) Assign Staff to a Booking");
+            System.out.println("4) Reschedule a Booking");
+            System.out.println("5) View Operational Insights Dashboard");
+            System.out.println("0) Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            //switch vars
+            String date;
+            String service;
+            int booking_id;
+
+            switch (choice) {
+
+                case 1:
+                    /// i lowkey dont remember who this is
+                    System.out.println("\n--- View and/or Process Booking Requests ---");
+                case 2:
+                    /// Elisha (Option 2)
+                    System.out.println("\n--- Creating New Booking ---");
+                    System.out.println("1.a) Input service and date wanted:\n");
+                    service = scanner.nextLine();
+                    System.out.println("Input date wanted (specify format):\n");
+                    date = scanner.nextLine();
+                    /// query the availabilities
+                    System.out.println("Enter preferred slot:\n");
+                    int slot = scanner.nextInt();
+                    ///  Update DB
+                    break;
+
+                case 3:
+                    System.out.println("\n--- Assigning Staff To a Booking ---");
+                    /// query all bookings
+                    /// or enter bookings nb? depends how we want to implement it
+                    booking_id  = scanner.nextInt();
+                    ///  query employees available at a specific session
+                    /// Update DB
+                    break;
+
+                case 4:
+                    System.out.println("\n--- Rescheduling Booking ---");
+                    /// query all bookings
+                    /// or enter bookings nb? depends how we want to implement it
+                    booking_id  = scanner.nextInt();
+                    /// query "near" bookings and show potential booking alternatives
+                    /// update DB
+                    break;
+
+                case 5:
+                    System.out.println("\n--- Operational Insights ---");
+                    int nestedChoice;
+                    System.out.println("\n1) View Bookings By Service");
+                    System.out.println("\n2) View Studio Utilization");
+                    System.out.println("\n3) View Employee Workload");
+                    System.out.println("\n4) View Busiest Working Periods");
+                    nestedChoice = scanner.nextInt();
+                    switch(nestedChoice){
+                        case 1:
+                            System.out.println("\n--- Bookings By Service ---");
+                            break;
+                        case 2:
+                            System.out.println("\n--- Studio Utilization ---");
+                            break;
+                        case 3:
+                            System.out.println("\n--- Employee Workload ---");
+                            break;
+                        case 4:
+                            System.out.println("\n--- Busy Working Periods ---");
+                            break;
+                        default:
+                            System.out.println("Invalid Option Selected");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+
+        } while (choice != 0);
+
+        scanner.close();
 
       // Finally but importantly close the statement and connection
       statement.close ( ) ;
