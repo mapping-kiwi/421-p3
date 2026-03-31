10d3bc78856f80e0381a609d79c27be67484f860
mira-muti <149914489+mira-muti@users.noreply.github.com>
2026-03-30 20:54:59 -0400

    pushing all files

---
+++ stored_procedures/index.sql
@@ -0,0 +1,5 @@
+-- =========================================
+-- INDEXING SECTION
+-- =========================================
+CREATE INDEX idx_workload_lookup ON isAssigned (employee_no, slot_id);
+
